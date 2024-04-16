657693da75842317cc8f3414b688188aafb8273c
Mikhail Shipov <mihail.korablev@gmail.com>
2024-04-16 18:55:50 +0300

    noise

--- js/init.js
+++ js/init.js
@@ -175,6 +175,45 @@ $("a.buttons:nth-last-child(1)").click(function () {
   $('a.buttons').toggleClass("hovered");
 });
 
+
+var canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d');
+
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+resize();
+window.onresize = resize;
+
+function noise(ctx) {
+    
+    var w = ctx.canvas.width,
+        h = ctx.canvas.height,
+        idata = ctx.createImageData(w, h),
+        buffer32 = new Uint32Array(idata.data.buffer),
+        len = buffer32.length,
+        i = 0;
+
+    for(; i < len;)
+        buffer32[i++] = ((4000 * Math.random())|0) << 30;
+    
+    ctx.putImageData(idata, 0, 0);
+}
+
+var toggle = true;
+
+// added toggle to get 30 FPS instead of 60 FPS
+(function loop() {
+    toggle = !toggle;
+    if (toggle) {
+        requestAnimationFrame(loop);
+        return;
+    }
+    noise(ctx);
+    requestAnimationFrame(loop);
+})();
+
 // const items = document.querySelectorAll('.case-item')
 // items.forEach(item => item.addEventListener("click", event => {
 //   event.target.classList.toggle('open')
